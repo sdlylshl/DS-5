84973d1b66a7eb71b7fdafc90a5f625f79848b78
sdlylshl <sdlylshl@163.com>
2015-04-09 08:41:23 +0800

    V8.5 OK 完成485面板功能设计

--- STM32/STM32F103/Source/panel/panel.c
+++ STM32/STM32F103/Source/panel/panel.c
@@ -16,13 +16,13 @@
 #define POLL_CMD 				0
 #define SET_USER_PASSWD			0x2
 #define SET_ADMIN_PASSWD		0x3
-#define ARM_CMD				0x4
+#define ARM_CMD					0x4
 
 #define POLL_ARM_RSP 			0x50
 #define POLL_CH_MAP_RSP			0x51
 #define SET_USER_PASSWD_RSP		0x52
-#define SET_ADMIN_PASSWD_RSP		0x53
-#define ARM_CMD_RSP			0x54
+#define SET_ADMIN_PASSWD_RSP	0x53
+#define ARM_CMD_RSP				0x54
 
 
 #define PANEL_CMD_HEAD 0xFC
@@ -40,13 +40,14 @@ uint16_t last_key = 0;
 //布防状态
 //A 0 布防 B 1 撤防 C 2 取消报警状态 
 volatile uint8_t PanelStatus = 0;
+volatile uint8_t lastpanelstatus = 0;
 uint8_t LastStatus = 0xFF;
 //****用来监视 PanelStatus 状态改变是否是用户操作
 //当SyncFlag不为0 则代表 用户进行相关配置
 //将状态 上传之后 清零
 // 1.有布撤防状态改变 2.设置通道映射
 uint8_t SyncFlag = 0;
-
+uint8_t SuccessFlag = 0;
 uint8_t bufangFlag = 0;
 uint8_t tongdaoFlag = 0;
 //密码相关
@@ -111,12 +112,74 @@ static void ansbufang(PANELCMD_t * pan){
 	Delay_ms(1);
 	RS485_RX();
 }
+void panelcmdpoll(PANELCMD_t * panelcmd){
+	static uint8_t lastId;
+
+	static uint8_t cmdbufangId;
+	static uint8_t cmdtongdaoId;
+	if (lastId == panelcmd->id)
+	{
+		//重发			
+		if (lastId == cmdbufangId)
+		{
+			panelcmd->data[0] = PanelStatus;
+			ansbufang(panelcmd);
+		}
+		else if (lastId == cmdtongdaoId)
+		{
+
+		}
+	}
+	else
+	{
+		//收到新指令
+		lastId = panelcmd->id;
+		//为了保证能有效的判断 cmdId 所以 如果主机发送过来的Id为0 做心跳处理
+		if ((SyncFlag & 0x01) && lastId)
+		{
+
+			panelcmd->data[0] = PanelStatus;
+			ansbufang(panelcmd);
+			SyncFlag &= 0xFE;
+			cmdbufangId = panelcmd->id;
+
+		}
+		else if ((SyncFlag & 0x02) && lastId){
+
+			SyncFlag &= 0xFD;
+			cmdtongdaoId = panelcmd->id;
+
+		}
+		else{
+			if (PanelStatus == 2){
+				//取消报警状态成功 切换到原状态
+				if (panelcmd->data[0] == 0xFD){
+					PanelStatus = 3;
+				}
+				else if (panelcmd->data[0] == 0xFE){
+					PanelStatus = 1;
+				}
+				else if (panelcmd->data[0] == 0xFF){
+					PanelStatus = 0;
+				}
+			}
+			if (cmdbufangId){ cmdbufangId = 0; }
+			if (cmdtongdaoId){ cmdtongdaoId = 0; }
+			panelcmd->data[0] = 0xFF;
+			ansbufang(panelcmd);
+
+		}
+
+
+	}
+
+}
 //接收布防状态
 static void recvbuchefang(PANELCMD_t * pan){
 	switch (pan->data[0])
 	{
-//-------------------------------------------------
-//服务器端更新数据
+		//-------------------------------------------------
+		//服务器端更新数据
 	case 0://布防
 		//PanelStatus = 0;
 		//break;		
@@ -133,11 +196,11 @@ static void recvbuchefang(PANELCMD_t * pan){
 
 		//收到应答
 		pan->data[0] = 0xff;
-		ansbufang(pan);	
+		ansbufang(pan);
 		//wavplay(yuanchengbufang);
 		break;
-//---------------------------------------------------
-//网关当前状态，正常主机查询指令
+		//---------------------------------------------------
+		//网关当前状态，正常主机查询指令
 	case 0xFD:
 		//判断是否是首次开机 同步布取消报警状态
 		if (firststart){
@@ -156,54 +219,9 @@ static void recvbuchefang(PANELCMD_t * pan){
 			PanelStatus = 0;
 			firststart = 0;
 		}
+		panelcmdpoll(pan);
+		//printf("sendId%03x\n", pan->id);
 
-		printf("sendId%03x\n", pan->id);
-		if ((bufangFlag == pan->id) && pan->id)
-		{
-			//布防设置失败
-			pan->data[0] = PanelStatus;
-			ansbufang(pan);
-			SyncFlag &= 0xFE;
-		}
-		else if ((tongdaoFlag == pan->id) && pan->id)
-		{
-			//通道设置失败
-			SyncFlag &= 0xFD;
-
-
-		}else{
-			
-			if ((bufangFlag)){
-				//布防成功
-				if(PanelStatus == 2){
-					//取消报警状态成功 切换到布防状态
-					PanelStatus =0;
-				}
-				bufangFlag = 0;
-			}
-			else	if ((tongdaoFlag)){
-				tongdaoFlag = 0;
-			}
-			//因为ID 改变了 所以 一定是数据接收成功了
-			//lastId = pan->id;
-			
-			//新指令
-			if ((SyncFlag & 0x01) && pan->id){
-				SyncFlag &= 0xFE;
-				pan->data[0] = PanelStatus;
-				ansbufang(pan);
-				bufangFlag = pan->id;
-			}else	if ((SyncFlag & 0x02) && pan->id){
-				//通道设置
-				SyncFlag &= 0xFD;
-				tongdaoFlag = pan->id;
-			}
-			else{
-				//普通应答 心跳
-				pan->data[0] = 0xFF;
-				ansbufang(pan);
-			}
-		}
 		break;
 
 	default:
@@ -265,22 +283,12 @@ void panel_paredata(){
 	}
 }
 //////////////////按键处理//////////////////////////////
-uint8_t cmppass(){
-	uint8_t i = 0;
-	for (i = 1; userpasswd[i]; ++i)
-	{
-		if (passwd[i] != userpasswd[i]){
-			return 0;
-		}
-	}
-	return 1;
-}
 uint8_t keyindex;
 uint8_t checkpasswd(){
-	if (keyindex == 4 )
+	if (keyindex == 4)
 	{
 		uint8_t i = 0;
-		for (i = 0; i< 4; i++)
+		for (i = 0; i < 4; i++)
 		{
 			if (passwd[i] != userpasswd[i]){
 				return 0;
@@ -292,44 +300,48 @@ uint8_t checkpasswd(){
 }
 void keyAevent(void){
 
-	SyncFlag |=1;
+	SyncFlag |= 1;
 	PanelStatus = 0;
 	keyindex = 0;
 }
-void keyATimeoutevent(void){ keyindex = 0; }
+void keyATimeoutevent(void){
+	SyncFlag |= 1;
+	PanelStatus = 0;
+	keyindex = 0;
+}
 void keyBevent(void){
 	if (checkpasswd())
 	{
-		
-		SyncFlag |=1;
+
+		SyncFlag |= 1;
 		PanelStatus = 1;
 	}
 
 	keyindex = 0;
 }
-void keyBTimeoutevent(void){ 
-keyindex = 0; 
+void keyBTimeoutevent(void){
+	keyindex = 0;
 }
 void keyCevent(void){
 
-			SyncFlag |=1;
+	SyncFlag |= 1;
+	lastpanelstatus = PanelStatus;
 	PanelStatus = 2;
-
 	keyindex = 0;
 }
 void keyCTimeoutevent(void){
-		SyncFlag |=1;
+	SyncFlag |= 1;
 	PanelStatus = 2;
 	keyindex = 0;
 }
 void keyNevent(void){
-	if (keyindex<16)
+	if (keyindex < 16)
 	{
 		passwd[keyindex++] = key;
 	}
 }
 void keyNTimeoutevent(void){
-	if (keyindex<16)
+	if (keyindex < 16)
 	{
 		passwd[keyindex++] = key;
 	}
@@ -337,7 +349,7 @@ void keyNTimeoutevent(void){
 void(*normalhandler)(void) = (void(*)(void))0;
 void(*timehandler)(void) = (void(*)(void))0;
 uint16_t panel_keyHandle(){
-
+	uint32_t timescan;
 	if (GetKey())
 	{
 		switch (key)
@@ -345,87 +357,30 @@ uint16_t panel_keyHandle(){
 		case KEY_A:
 			normalhandler = keyAevent;
 			timehandler = keyATimeoutevent;
+			timescan = 1000;
 			break;
 		case KEY_B:
 			normalhandler = keyBevent;
 			timehandler = keyBTimeoutevent;
+			timescan = 1000;
 			break;
 		case KEY_C:
 			normalhandler = keyCevent;
 			timehandler = keyCTimeoutevent;
+			timescan = 1000;
 			break;
 		default:
 			normalhandler = keyNevent;
 			timehandler = keyNTimeoutevent;
+			timescan = 4000;								//调节用户体验
 			break;
 		}
 		//二选一 
-		keyEvent(normalhandler, timehandler, 5000);
+		keyEvent(normalhandler, timehandler, timescan);
 	}
 	return 0;
 }
-uint16_t panel_getbufang(){
-	uint8_t i;
-	static uint8_t id = 0;
-	static uint8_t keymflg = 0;
-	static uint8_t modeset = 0;		//布防模式设置
-	uint32_t timeout = 8000;		//超时时间
-	uint16_t ckey;
-	if (timeout-- == 0){
-		last_key = 0;
-		timeout = 100000;
-	}
-	//ckey=sizeof(passwd);
 
-	ckey = KeyScan();
-	if (ckey && (ckey != last_key))
-	{
-		last_key = ckey;
-
-		if (ckey == KEY_A){
-			PanelStatus = 0;
-			SyncFlag |= 0x01;
-			id = 0;
-		}
-		else if (ckey == KEY_B){
-			if (id && cmppass()){
-				PanelStatus = 1;
-				SyncFlag |= 0x01;
-			}
-			id = 0;
-		}
-		else if (ckey == KEY_C){
-
-			id = 0;
-			PanelStatus = 2;
-			SyncFlag |= 0x01;
-			//先做功能键的处理		
-		}
-		else
-			//密码处理
-		if (id){
-
-			if (id < (sizeof(userpasswd) / sizeof(userpasswd[0]))){
-				passwd[id++] = ckey;
-			}
-			else{
-				id = 0;
-			}
-		}
-		else if (userpasswd[0] == ckey)
-		{
-			//进入密码状态机
-			passwd[0] = ckey;
-			for (i = 1; i < (sizeof(passwd) / sizeof(passwd[0])); i++)
-			{
-				passwd[i] = 0;
-			}
-			id = 1;
-		}
-		//密码处理结束
-
-	}
-}
 //////////////////状态指示//////////////////////////////
 void panel_ShowStatus(){
 
@@ -449,7 +404,7 @@ void panel_ShowStatus(){
 			Write_String(0xc0, "qu xiao bao jing");
 			LEDFlashing(500);
 			break;
-		case 3://取消报警状态
+		case 3://取消
 
 			Write_String(0xc0, "elin            ");
 			LEDFlashing(500);
@@ -471,7 +426,6 @@ void panel(void) {
 
 	while (1) {
 		panel_ShowStatus();
-		//panel_getbufang();
 		panel_keyHandle();
 		panel_paredata();
 
